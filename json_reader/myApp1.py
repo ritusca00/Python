b33a5d71738d7f0dc0f1409ca41fcb06eedefcae
ritusca00 <bliznikrita@hotmail.com>
2019-01-06 15:38:39 +0100

    json reader
    enter the word and a program find in json file

---
+++ json_reader/myApp1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 14:54:08 2019
+
+@author: blizn
+"""
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
